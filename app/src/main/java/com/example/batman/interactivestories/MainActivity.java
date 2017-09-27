e6f67eb4b37cf06c1ef00e4996b5ba70710f5489
darshan1504 <djethwa2810@gmail.com>
2017-09-26 18:24:16 -0700

    Added onclick functionality to display username

---
+++ app/src/main/java/com/example/batman/interactivestories/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.batman.interactivestories;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText nameField;
+    private Button startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameField = (EditText)findViewById(R.id.nameEditText);
+        startButton = (Button)findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameField.getText().toString();
+                Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
