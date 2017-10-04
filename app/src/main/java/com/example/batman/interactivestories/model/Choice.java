4b8e6e7ea9e37260d5d4b11c920ae2d6a4a6ef0d
darshan1504 <djethwa2810@gmail.com>
2017-10-04 14:58:52 -0700

    Added new activity

--- app/src/main/java/com/example/batman/interactivestories/model/Choice.java
+++ app/src/main/java/com/example/batman/interactivestories/model/Choice.java
@@ -2,37 +2,28 @@ package com.example.batman.interactivestories.model;
 
 public class Choice {
     private int textId;
-    private int imageId;
-    private Choice choice1;
-    private Choice choice2;
+    private int nextPage;
 
-    public int getTextId(){
+    public Choice(int textId, int nextPage) {
+        this.textId = textId;
+        this.nextPage = nextPage;
+    }
+
+    public int getTextId() {
         return textId;
     }
-    public void setTextId( int textId){
+
+    public void setTextId(int textId) {
         this.textId = textId;
     }
-    public int getImageId(){
-        return imageId;
-    }
-    public void setImageId( int imageId){
-        this.imageId = imageId;
+
+    public int getNextPage() {
+        return nextPage;
     }
 
-    public Choice getChoice1() {
-        return choice1;
-    }
 
-    public void setChoice1(Choice choice1) {
-        this.choice1 = choice1;
-    }
-
-    public Choice getChoice2() {
-        return choice2;
-    }
-
-    public void setChoice2(Choice choice2) {
-        this.choice2 = choice2;
+    public void setNextPage(int nextPage) {
+        this.nextPage = nextPage;
     }
 }
 
--- app/src/main/java/com/example/batman/interactivestories/model/Page.java
+++ app/src/main/java/com/example/batman/interactivestories/model/Page.java
@@ -2,9 +2,27 @@ package com.example.batman.interactivestories.model;
 
 public class Page {
     private int textId;
-    private int nextPage;
+    private int imageID;
+    private Choice choice1;
+    private Choice choice2;
+    private boolean isFinalPage = false;
+
+    public Page(int textId, int imageID) {
+        this.textId = textId;
+        this.imageID = imageID;
+        this.isFinalPage = true;
+    }
+
+    public Page(int textId, int imageID, Choice choice1, Choice choice2) {
+        this.textId = textId;
+        this.imageID = imageID;
+        this.choice1 = choice1;
+        this.choice2 = choice2;
+
+    }
 
     public int getTextId() {
+
         return textId;
     }
 
@@ -12,11 +30,27 @@ public class Page {
         this.textId = textId;
     }
 
-    public int getNextPage() {
-        return nextPage;
+    public int getImageID() {
+        return imageID;
     }
 
-    public void setNextPage(int nextPage) {
-        this.nextPage = nextPage;
+    public void setImageID(int imageID) {
+        this.imageID = imageID;
+    }
+
+    public Choice getChoice1() {
+        return choice1;
+    }
+
+    public void setChoice1(Choice choice1) {
+        this.choice1 = choice1;
+    }
+
+    public Choice getChoice2() {
+        return choice2;
+    }
+
+    public void setChoice2(Choice choice2) {
+        this.choice2 = choice2;
     }
 }
